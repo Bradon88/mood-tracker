28d314d7b5a72a096ef8f6897a90c090b5a6c9b1
Amanda Mattleman <amandamattleman@MSI>
2021-03-22 10:52:45 -0600

    chat-component

--- mood-tracker/src/Components/Chat/Chat.js
+++ mood-tracker/src/Components/Chat/Chat.js
@@ -1,11 +1,59 @@
 import React, { useState } from 'react'
 import axios from 'axios'
+import { makeStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 
 const Chat = (props) => {
-    const []
+    // const []
+
+    const useStyles = makeStyles((theme) => ({
+        formControl: {
+          margin: theme.spacing(1),
+          minWidth: 120,
+        },
+        selectEmpty: {
+          marginTop: theme.spacing(2),
+        },
+      }));
+
+     const classes = useStyles();
+      
+      
+      
+
 
     return <div>
-        
+        <FormControl className = {classes.formControl}
+        style={{
+            width: "25%",
+            display: "flex",
+            flexDirection: "column"
+        }}>
+            <InputLabel>Select Team</InputLabel>
+            <Select>
+                <MenuItem>Team1</MenuItem>
+                <MenuItem>Team2</MenuItem>
+                <MenuItem>Team3</MenuItem>
+            </Select>
+        </FormControl>
+
+        <FormControl className = {classes.formControl}
+         style={{
+            width: "25%",
+            display: "flex",
+            flexDirection: "column"
+        }}>
+            <InputLabel>Select Team Member</InputLabel>
+            <Select>
+                <MenuItem>Member1</MenuItem>
+                <MenuItem>Member2</MenuItem>
+                <MenuItem>Member3</MenuItem>
+            </Select>
+        </FormControl>
     </div>
 }
 
--- mood-tracker/src/routes.js
+++ mood-tracker/src/routes.js
@@ -2,7 +2,7 @@ import { Switch, Route} from "react-router-dom";
 import Auth from './Components/Auth/Auth';
 import Login from './Components/Auth/Login'
 import Main from './Components/Main/Main'
-
+import Chat from './Components/Chat/Chat'
 const Routes = (props) =>{
    return(
 
@@ -10,6 +10,7 @@ const Routes = (props) =>{
          <Route path="/Auth" component={Auth}/>
          <Route path="/Login" component={Login}/>
          <Route path="/Main" component={Main}/>
+         <Route path="/Chat" component={Chat}/>
       </Switch>
    )
    }
