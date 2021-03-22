262711170bbcd7f0ea51932ff3673be8eb73f240
Amanda Mattleman <amandamattleman@MSI>
2021-03-22 10:58:02 -0600

    chat-component

--- mood-tracker/src/Components/Chat/Chat.js
+++ mood-tracker/src/Components/Chat/Chat.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { makeStyles } from '@material-ui/core/styles';
 import InputLabel from '@material-ui/core/InputLabel';
@@ -6,9 +6,29 @@ import MenuItem from '@material-ui/core/MenuItem';
 import FormHelperText from '@material-ui/core/FormHelperText';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
+import io from "socket.io-client"
 
 const Chat = (props) => {
-    // const []
+
+    const [user, setUser] = useState("")
+    const [message, setMessage] = useState("")
+    const [messages, setMessages] = useState([])
+    const [socket, setSocket] = useState(null)
+
+  useEffect(() => {
+    if (!socket) {
+      setSocket(io.connect())
+    }
+  }, [socket])
+
+  useEffect(() => {
+    if (socket) {
+      socket.on("receive-message", (body) => {
+        console.log(body)
+        setMessages((prevMessages) => [...prevMessages, body])
+      })
+    }
+  }, [socket])
 
     const useStyles = makeStyles((theme) => ({
         formControl: {
@@ -54,7 +74,23 @@ const Chat = (props) => {
                 <MenuItem>Member3</MenuItem>
             </Select>
         </FormControl>
+        <div>
+      <input value={user} onChange={(e) => setUser(e.target.value)} />
+      <p>This is the chat</p>
+      {messages.map((m) => (
+        <div>
+          {m.user}: {m.message}
+        </div>
+      ))}
+      <div>
+        <input value={message} onChange={(e) => setMessage(e.target.value)} />
+        <button onClick={() => socket.emit("send-message", { message, user })}>
+          TEST
+        </button>
+      </div>
     </div>
+    </div>
+    
 }
 
 export default Chat
