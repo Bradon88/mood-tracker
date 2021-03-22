19dca1f211f73815c359753452ba8651248a7cc6
Bradon Nichols <nichols.bradon@gmail.com>
2021-03-22 14:37:00 -0600

    landing page

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,6 +3,7 @@ import {Link} from 'react-router-dom'
 const Footer = () => {
     return (
         <div className='footer'>
+            <Link to='/' className='link'>Home</Link>
             <Link to='/Main' className='link'>Current Mood</Link>
             <Link to='/Chat' className='link'>Chat</Link>
             <Link to='/Register' className='link'>Register</Link>
--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,14 @@ import Login from './Components/Auth/Login';
 import Main from './Components/Main/Main';
 import Chat from './Components/Chat/Chat'
 import Breadcrumbs from './Components/Breadcrumbs/Breadcrumbs';
+import Home from './Components/Home'
 
 const Routes = (props) =>{
    return(
       <>
          <Breadcrumbs />
          <Switch>
+            <Route exact path='/' component={Home}/>
             <Route path="/Register" component={Register} render={props => <Register {...props} />}/>
             <Route path="/Login" component={Login} render={props => <Login {...props} />}/>
             <Route path="/Main" component={Main} render={props => <Main {...props} />}/>
