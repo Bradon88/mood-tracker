152edce5f458bb1b8fa675d40449383e839f2a87
Amanda Mattleman <amandamattleman@MSI>
2021-03-23 14:16:46 -0600

    moment imported

--- src/Components/Chart/Chart.js
+++ src/Components/Chart/Chart.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
 import {Line} from 'react-chartjs-2';
 import {Pie} from 'react-chartjs-2';
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
+import moment from 'moment'
 import './Chart.scss'
 
 class Chart extends Component {
     render(){
 
-        const data1 = {
+        const donutChart = {
             type: 'doughnut',
             datasets: [{
                 data: [10,20,30],
@@ -19,33 +20,28 @@ class Chart extends Component {
         }
 
 
-        const data = {
+        const lineChart = {
+            
             type: 'line',
-            labels: ['1', '2', '3', '4'],
+            labels: ['1', '2', '3', '4', '5'],
                 datasets: [{
-                    label: 'March',
+                    label: moment().format('MMMM YYYY'),
                     pointBorderColor: '#000',
-                    pointBackgroundColor: '#e76f51',
+                    borderColor: '#2a9d8f',
+                    fill: false,
+                    
+                    // pointBackgroundColor: '#e76f51',
                     pointBorderWidth: '1',
                     radius: '6',
                     data: [4, 3, 2, 5, 1]
                 }],
             
-            options: {
-                scales: {
-                    xAxes: [{
-                        
-                        type: 'linear',
-                        position: 'bottom'
-                    }]
-                }
-            }
         };
         return (
             <div><h1 style={{textAlign: 'center'}}>Moods on Display</h1>
             <Line
 
-            data={data}
+            data={lineChart}
             width={200}
             // height={300} for responsive/mobile design
             options={{
@@ -55,16 +51,14 @@ class Chart extends Component {
             }
             }}/>
             <Pie
-            data={data1}
+            data={donutChart}
             height={150}
             options={
                 {
                     display: true,
                     cutoutPercentage: '60'
                 }
-            }
-            
-            />
+            }/>
 
             <button className="btn"
             style={{
