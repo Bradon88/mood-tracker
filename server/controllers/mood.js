4085a7d282de179e6569b0e5245ceffc9333c61c
Christie Jensen <christiejensen@Christies-MacBook-Air.local>
2021-03-23 00:46:49 -0600

    trying to pull

---
+++ server/controllers/mood.js
@@ -0,0 +1,18 @@
+module.exports = {
+    addMood: async ( req, res ) => { 
+        const db = req.app.get('db')
+        if( req.session.user ){
+            const { mood, date, current_activity, activity_notes } = req.body
+            await db.mood.create_mood([ ])
+        }
+    },
+    getMood: async ( req, res ) => {
+        const db = req.app.get('db')
+        if( req.session.user ){
+            const mood = await db.mood.get_mood([ req.session.user.user_id ])
+            return res.status(200).send(mood)
+        } else {
+            return res.status(401).send('Please log in to view mood information.')
+        }
+    }
+}
