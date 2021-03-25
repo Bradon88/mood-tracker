1aa6fa8296750633fee9fe24ff608636e4b365ff
Bradon Nichols <nichols.bradon@gmail.com>
2021-03-25 10:05:44 -0600

    update

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,10 +1,11 @@
 import {Link} from 'react-router-dom'
 import {AuthContext} from '../Context/AuthContext';
 import { useState, useContext} from "react";
-import HomeIcon from '@material-ui/icons/Home';
+import HomeRoundedIcon from '@material-ui/icons/HomeRounded';
 import SentimentVerySatisfiedIcon from '@material-ui/icons/SentimentVerySatisfied'
 import ChatBubbleIcon from '@material-ui/icons/ChatBubble';
 import PowerSettingsNewIcon from '@material-ui/icons/PowerSettingsNew';
+import PeopleIcon from '@material-ui/icons/People';
 
 const Footer = () => {
 
@@ -15,9 +16,10 @@ const Footer = () => {
             null
         :
             <div className='footer'>
-                <Link to='/' className='footer-links'><HomeIcon/></Link>
-                <Link to='/Main' className='footer-links'><SentimentVerySatisfiedIcon/></Link>
+                <Link to='/' className='footer-links'><HomeRoundedIcon/></Link>
                 <Link to='/Chat' className='footer-links'><ChatBubbleIcon/></Link>
+                <Link to='/Main' className='footer-links'><SentimentVerySatisfiedIcon/></Link>
+                <Link to='/Team' className='footer-links'><PeopleIcon/></Link>
                 <button onClick={() => logout()} className='footer-logout'><PowerSettingsNewIcon/></button>
             </div>
         }
