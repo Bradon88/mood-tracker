ec58c59ef0553d7feb3d7ee63b3065b56ed6fff5
Bradon Nichols <nichols.bradon@gmail.com>
2021-03-26 09:19:05 -0600

    update

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -10,29 +10,29 @@ const Home = () => {
             </div>
             
             <div className='landing-page-text-parent'>
-                <h2 className='landing-page-text'>
+                <h3 className='landing-page-text'>
                 Get simple and useful insight into yourself! 
-                </h2>
-                <div className='landing-page-text-parent'>
-                    <h3 className='landing-page-text'>
-                    Get simple and useful insight into yourself! 
-                    </h3>
-                    <h3 className='landing-page-text'>
-                    Track your daily emotional health. 
-                    </h3>
-
-                    <h3 className='landing-page-text'>
-                    Identify any patterns or possible triggers.
-                    </h3>
-
-                    <h3 className='landing-page-text'>
-                    For all the ups and the downs!
-                    </h3>
+                </h3>
                 
-                    <Link to='/Register'className='landing-page-link'>Register for an Account</Link>
-                    <Link to='/Login'className='landing-page-link'>Login</Link>
-                </div>
-        </div>
+                <h3 className='landing-page-text'>
+                Get simple and useful insight into yourself! 
+                 </h3>
+                <h3 className='landing-page-text'>
+                Track your daily emotional health. 
+                </h3>
+
+                <h3 className='landing-page-text'>
+                 Identify any patterns or possible triggers.
+                </h3>
+
+                <h3 className='landing-page-text'>
+                For all the ups and the downs!
+                 </h3>
+                
+                 <Link to='/Register'className='landing-page-link'>Register for an Account</Link>
+                <Link to='/Login'className='landing-page-link'>Login</Link>
+                
+            </div>
     </div>
     )
 }
