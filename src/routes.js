b24eaf7618e19fe8e3ce73055a8c098e24031aec
mrkaptep <kaptep@yahoo.com>
2021-03-30 17:02:45 -0600

    protected routes

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,18 @@
-import { Switch, Route} from "react-router-dom";
+import { Switch, Route, Redirect} from "react-router-dom";
+import React, {useContext} from 'react';
+import {AuthContext} from './Context/AuthContext'
 import Register from './Components/Auth/Register';
 import Login from './Components/Auth/Login';
 import Main from './Components/Main/Main';
-import Chat from './Components/Chat/Chat'
-import CurrentMood from './Components/CurrentMood/CurrentMood'
 import Breadcrumbs from './Components/Breadcrumbs/Breadcrumbs';
 import Home from './Components/Home/Home'
-import Team from './Components/Team/Team'
-import Notes from './Components/Notes/Notes'
-import AddNotes from './Components/Notes/AddNotes'
+
+
+
 
 const Routes = (props) =>{
+   const {user} = useContext(AuthContext)
+
    return(
       <>
          {/* <Breadcrumbs /> */}
@@ -18,12 +20,15 @@ const Routes = (props) =>{
             <Route exact path='/' component={Home}/>
             <Route path="/Register" component={Register} render={props => <Register {...props} />}/>
             <Route path="/Login" component={Login} render={props => <Login {...props} />}/>
+            {!user ? (
+               <Redirect
+                  to={{
+                     pathname: "/",
+                  }}
+               />
+            ): null}
             <Route path="/Main" component={Main} render={props => <Main {...props} />}/>
-            <Route path="/Chat/:room?" component={Chat}/>
-            <Route path="/CurrentMood" component={CurrentMood}/>
-            <Route path="/Team" component={Team}/>
-            <Route path='/Notes' component={Notes}/>
-            <Route path='/AddNotes' component={AddNotes}/>
+            
          </Switch>
       </>
    )
